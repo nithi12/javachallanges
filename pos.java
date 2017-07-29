fd29b3eb352e43359cfe983146c1779f8036c781
nithi12 <30543432+nithi12@users.noreply.github.com>
2017-07-29 10:44:25 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class neg
+{
+public static void main(String a[])
+{
+Scanner s=new Scanner(System.in);
+int a,b,c;
+if(a>0)
+{
+System.out.print("the no is positive");
+}
+else if(b<0)
+{
+System.out.print("the no is negative");
+}
+else
+{
+System.out.print("the given no is zero");
+}
+}
+}
