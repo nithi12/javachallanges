d41ed595f089a9040fbe1edb81745a121b33be95
nithi12 <30543432+nithi12@users.noreply.github.com>
2017-08-28 05:57:26 -0700

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class largest
+{
+public static void main(String args[])
+{
+int x,y,z;
+System.out.println('entered three numbers")
+Scanner scan=new Scanner(System.in);
+x=scan.nextInt();
+y=scan.nextInt();
+z=scan.nextInt();
+if(x>y&&x>z)
+{System.out.println("x is largest");
+}
+else if(y>x&&y>z)
+{
+System.out.println("y is largest");
+}
+else
+{
+System.out.println("z is largest");
+}
+}
+}
