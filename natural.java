d6d989c83e70c91288b01e940ba13b9e7b7c6d1c
nithi12 <30543432+nithi12@users.noreply.github.com>
2017-08-28 06:24:55 -0700

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*l
+class natural numbers
+{
+public static void main(String a[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number");
+int num=sc.nextInt();
+sum=0;
+for(int i=0;i<=num;i++)
+{
+sum=sum+i;
+}
+System.out.println("sum="+sum);
+}
+}
