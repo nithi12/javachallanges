a877a6cca5228abf460736c62a26cbfc87758383
nithi12 <30543432+nithi12@users.noreply.github.com>
2017-09-02 21:33:22 -0700

    Update noofdigits.java

--- noofdigits.java
+++ noofdigits.java
@@ -1,26 +1,13 @@
-import java.util.Scanner;
-class Numofdigit 
+import java.io.*;
+import java.util.*;
+class numojdig
 {
-public static void main(String[] args) 
-{
- int no,a=0;
- Scanner s=new Scanner(System.in);
-System.out.println("Enter any number : ");
- no = s.nextInt();
-
-if(no<0)
-{
-no=no * -1;
-}
-else if(no==0)
-{
-no=1;
-}
-while(no>0)
-{
-no=no/10;
-a++;
-}
-System.out.println("Number of Digits in given number is: "+a);
-}
-}
+ public static void main(String a[])
+ {
+  int count=0,num=n;
+  for(;num!=0num/=10,++count)
+  {
+   System.out.println("number of digits"+count);
+  }
+ }
+  
