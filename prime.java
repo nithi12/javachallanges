fe71dac3e28a4eda3e551154b71364dbe2e30cf9
nithi12 <30543432+nithi12@users.noreply.github.com>
2018-06-19 12:06:45 +0530

    prime.java
    good

--- prime.java
+++ prime.java
@@ -2,23 +2,30 @@ import java.io.*;
 import java.util.*;
 class prime
 {
-public static void main(String a[])
-{
-int num=n;
-boolean flag=false;
-for(int i=2;i<=num/2;++i)
-{
-if(num%i==0){
-flag=true;
-break;
-}
-if(!flag)
-{
-System.out.println(num+"is a prime number");
-}
-else
-{
-System.out.println(num+"is not a prime number");
-}
-}
+  public static void main(String arg[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int s= sc.nextInt();
+    int flag=0,m=s/2;
+    if(s==0||s==1)
+    {
+      System.out.print("it's not a prime number");
+    }
+    else
+    {
+      for(int i=0;i<m;i++)
+      {
+        if(s%2==0)
+        {
+          System.out.print("not a prime no");
+          flag=1;
+          break;
+        }
+      }
+      if(flag==0)
+      {
+        System.out.print("yes");
+      }
+    }
+  }
 }
