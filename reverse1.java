519b5ac2e206b70066da0bab65b55805e087d3ae
nithi12 <30543432+nithi12@users.noreply.github.com>
2018-06-19 21:58:42 +0530

    Create reverse1.java

---
+++ reverse1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class reverse1
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+StringBuffer s1=new StringBuffer(s).reverse();
+System.out.print(s1);
+}
+}
+}
