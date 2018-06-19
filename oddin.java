0e99e4e3c6c63b46b99db3a850b91b7b07aba689
nithi12 <30543432+nithi12@users.noreply.github.com>
2018-06-19 12:12:05 +0530

    Create oddin.java

---
+++ oddin.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class oddin
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(ystem.in);
+int s=sc.nextInt();
+int a[]=new int[s];
+for(int i=0;i<s;i++)
+{
+a[i]=sc.nextInt();
+if(a[i]%2==0)
+{
+ System.out.print("even");
+ }
+ else
+ {
+ System.out.print(a[i]);
+ }
+ }
+ }
+ }
