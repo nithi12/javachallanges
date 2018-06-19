6a177ab47c8d3f692475235172347964b35447a4
nithi12 <30543432+nithi12@users.noreply.github.com>
2018-06-19 18:10:04 +0530

    Create between.java

---
+++ between.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class between
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int s=sc.nextInt();
+int a[]=new int[s];
+if(s==1||s==0)
+{
+System.out.print("not a prime");
+}
+for(int i=0;i<s;i++)
+{
+a[i]=sc.nextInt();
+for(int j=0;j<
