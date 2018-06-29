dc33c8691230e032296e8b80dcb158150d395bb4
nithi12 <30543432+nithi12@users.noreply.github.com>
2018-06-29 21:54:00 +0530

    Update mul.java

--- mul.java
+++ mul.java
@@ -4,11 +4,13 @@ class mul
 {
 public static void main(String arg[])
 {
-int s=5;
+  Scanner sc=new Scanner(System.in);
+int s=sc.nextInt();
+  System.out.print(s);
 int n=0;
 for(int i=1;i<5;i++)
 {
-n=s+5;
+n=s+s;
 }
 System.out.print(n);
 }
