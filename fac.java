5c5780db2b63b0d46e7a08af65f157db53e2873b
nithi12 <30543432+nithi12@users.noreply.github.com>
2018-06-29 21:40:24 +0530

    Create fac.java

---
+++ fac.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class fac
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int s=sc.nextInt();
+int num=1,num=0;
+for(int i=s;i>0;i--)
+{
+num=num*i;
+}
+System.out.print(num);
+}
+}
