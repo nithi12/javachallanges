b0899d6d4499d327941251a31de8cd972afdb447
nithi12 <30543432+nithi12@users.noreply.github.com>
2018-07-01 09:58:06 +0530

    Create period.java

---
+++ period.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class period 
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+String stl=sc.nextLine();
+StringBuffer st1=new StringBuffer(stl);
+String st=String.valueOf(st1.append("."));
+System.out.print(st);
+}
+}
